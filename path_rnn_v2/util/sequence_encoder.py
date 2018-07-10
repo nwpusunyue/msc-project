10b23f7b6a5a8e34a5a45fb1bdce1d637374a531
silvianacmp <silviana_cimpian@yahoo.com>
2018-07-10 12:36:30 +0100

    Add additive lstm attention module in the sequence encoder.

--- path_rnn_v2/util/sequence_encoder.py
+++ path_rnn_v2/util/sequence_encoder.py
@@ -1,10 +1,9 @@
-import os
-
 import numpy as np
 import tensorflow as tf
 
 from path_rnn_v2.util import rnn
 from path_rnn_v2.util.activations import activation_from_string
+from path_rnn_v2.util.attention import attend_sequence
 from path_rnn_v2.util.ops import extract_axis_1, replace_val
 
 
@@ -38,6 +37,11 @@ def encoder(sequence, seq_length, repr_dim=100, module='lstm', name='encoder', r
         elif module == 'gru':
             # [batch_size, repr_dim x 2]
             out = bi_rnn(repr_dim, tf.contrib.rnn.GRUBlockCell(repr_dim), sequence, seq_length, **extra_args)
+        elif module == 'additive_attention':
+            # [batch_size, max_seq_len, repr_dimx x 2]
+            out = bi_lstm(repr_dim, sequence, seq_length, last_output=False, **extra_args)
+            # [batch_size, repr_dim x 2]
+            out, _ = attend_sequence(out, seq_length, module='additive')
         elif module == 'dense':
             # [batch_size, repr_dim x 2]
             out = tf.layers.dense(sequence, repr_dim)
@@ -68,14 +72,23 @@ def encoder(sequence, seq_length, repr_dim=100, module='lstm', name='encoder', r
 
 # RNN Encoders
 def _bi_rnn(size, fused_rnn, sequence, seq_length, with_projection=False, projection_activation=None,
-            with_backward=True):
+            with_backward=True, last_output=True):
     output = rnn.fused_birnn(fused_rnn, sequence, seq_length, with_backward=with_backward,
                              dtype=tf.float32, scope='rnn')[0]
-    if with_backward:
-        output = tf.concat([extract_axis_1(output[0], seq_length - 1),
-                            extract_axis_1(output[1], seq_length - 1)], 1)
+
+    if last_output:
+        if with_backward:
+            # [batch_size, repr_dim x 2]
+            output = tf.concat([extract_axis_1(output[0], seq_length - 1),
+                                extract_axis_1(output[1], seq_length - 1)], axis=1)
+        else:
+            # [batch_size, repr_dim]
+            output = extract_axis_1(output, seq_length - 1)
     else:
-        output = extract_axis_1(output, seq_length - 1)
+        if with_backward:
+            # [batch_size, max_seq_len, repr_dim x 2]
+            output = tf.concat([output[0], output[1]], axis=2)
+
     if with_projection:
         projection_initializer = tf.constant_initializer(np.concatenate([np.eye(size), np.eye(size)]))
         projection_activation = activation_from_string(
@@ -85,14 +98,17 @@ def _bi_rnn(size, fused_rnn, sequence, seq_length, with_projection=False, projec
     return output
 
 
-def bi_lstm(size, sequence, seq_length, with_projection=False, projection_activation=None, with_backward=True):
+def bi_lstm(size, sequence, seq_length, with_projection=False, projection_activation=None, with_backward=True,
+            last_output=True):
     return _bi_rnn(size, tf.contrib.rnn.LSTMBlockFusedCell(size), sequence, seq_length, with_projection,
-                   projection_activation, with_backward)
+                   projection_activation, with_backward, last_output)
 
 
-def bi_rnn(size, rnn_cell, sequence, seq_length, with_projection=False, projection_activation=None, with_backward=True):
+def bi_rnn(size, rnn_cell, sequence, seq_length, with_projection=False, projection_activation=None, with_backward=True,
+           last_output=True):
     fused_rnn = tf.contrib.rnn.FusedRNNCellAdaptor(rnn_cell, use_dynamic_rnn=True)
-    return _bi_rnn(size, fused_rnn, sequence, seq_length, with_projection, projection_activation, with_backward)
+    return _bi_rnn(size, fused_rnn, sequence, seq_length, with_projection, projection_activation, with_backward,
+                   last_output)
 
 
 if __name__ == '__main__':
@@ -107,12 +123,14 @@ if __name__ == '__main__':
         for j in range(test_seq_length[i]):
             test_sequence[i, j, :] = j + 1
 
-    sequence = tf.placeholder(tf.float32, shape=[None, None, input_dim])
+    sequence = tf.placeholder(tf.float32, shape=[None, max_sequence_length, input_dim])
+    unit_sequence = tf.placeholder(tf.float32, shape=[None, 1, input_dim])
     seq_length = tf.placeholder(tf.int32, shape=[None])
 
     avg_encoded = encoder(sequence, seq_length, module='average', name='average')
     gru_encoded = encoder(sequence, seq_length, module='gru', repr_dim=repr_dim, name='gru')
-    identity_encoded = encoder(sequence, seq_length, module='identity', name='identity')
+    identity_encoded = encoder(unit_sequence, seq_length, module='identity', name='identity')
+    attention_encoded = encoder(sequence, seq_length, repr_dim=repr_dim, module='additive_attention', name='attention')
 
     with tf.train.MonitoredTrainingSession() as sess:
         test_encoded = sess.run(avg_encoded,
@@ -133,6 +151,15 @@ if __name__ == '__main__':
         assert test_encoded.shape == (batch_size, repr_dim * 2)
         assert (test_encoded[2, :] == 0).all()
 
+        test_encoded = sess.run(attention_encoded,
+                                feed_dict={
+                                    sequence: test_sequence,
+                                    seq_length: test_seq_length
+                                })
+
+        assert (test_encoded.shape == (batch_size, repr_dim * 2))
+        assert (test_encoded[2] == 0.0).all()
+
         test_sequence = np.zeros([batch_size, 1, input_dim])
         test_seq_length = np.ones([batch_size])
 
@@ -141,7 +168,7 @@ if __name__ == '__main__':
 
         test_encoded = sess.run(identity_encoded,
                                 feed_dict={
-                                    sequence: test_sequence,
+                                    unit_sequence: test_sequence,
                                     seq_length: test_seq_length
                                 })
 
