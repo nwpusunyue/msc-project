6358e5d377e2d11643663c6c630172559c3cfb3b
silvianacmp <silviana_cimpian@yahoo.com>
2018-07-20 13:01:50 +0100

    Wrap tokenizer in a class extending the abstract base NLTK tokenizer class.

--- parsing/genia/word_tokenize.py
+++ parsing/genia/word_tokenize.py
@@ -1,18 +1,27 @@
 import subprocess
 
+from nltk.tokenize.api import TokenizerI
 from shlex import quote
 
 GTB_PATH = '~/bionlp_st_2013_supporting/tls/GTB-tokenize.pl'
 
 
-def genia_word_tokenize(text):
-    output = subprocess.check_output('echo {} | {}'.format(quote(text), GTB_PATH),
-                                     executable="/bin/bash",
-                                     shell=True)
-    output = output.decode('utf-8').strip()
-    output = output.split(' ')
-    return output
+class GTBTokenizer(TokenizerI):
+
+    def __init__(self):
+        pass
+
+    def tokenize(self, text):
+        output = subprocess.check_output('echo {} | {}'.format(quote(text), GTB_PATH),
+                                         executable="/bin/bash",
+                                         shell=True)
+        output = output.decode('utf-8').strip()
+        output = output.split(' ')
+        return output
+
+    def span_tokenize(self, s):
+        pass
 
 
 if __name__ == '__main__':
-    print(genia_word_tokenize("Let's tokenize the hell out of this!"))
+    print(GTBTokenizer().tokenize("Let's tokenize the hell out of this!"))
