619dbe1db16cd455945d811365dd17e0a32f4904
silvianacmp <silviana_cimpian@yahoo.com>
2018-08-10 11:04:36 +0100

    Document relation models

--- path_rnn_v2/experiments/document_relation_models/attention_document_relation.py
+++ path_rnn_v2/experiments/document_relation_models/attention_document_relation.py
@@ -22,13 +22,13 @@ def model_params_generator(max_path_len, max_rel_len, max_ent_len, word2vec_embe
             'reuse': False
         },
         'relation_encoder_params': {
-            'module': 'average',
-            'name': 'relation_average_encoder',
+            'module': 'additive_attention',
+            'name': 'attention_lstm_encoder',
             'repr_dim': emb_dim,
             'activation': None,
             'dropout': None,
             'extra_args': {
-                'with_backwards': False,
+                'with_backward': True,
                 'with_projection': False
             }
         },
@@ -45,6 +45,7 @@ def model_params_generator(max_path_len, max_rel_len, max_ent_len, word2vec_embe
             'repr_dim': emb_dim,
             'module': 'lstm',
             'name': 'path_encoder',
+            'activation': None,
             'dropout': dropout,
             'extra_args': {
                 'with_backward': False,
@@ -63,7 +64,7 @@ if __name__ == '__main__':
     visible_devices = '1'
     memory_fraction = 0.5
 
-    model_name = 'baseline_document_relation'
+    model_name = 'attention_document_relation'
     extra_parser_args_adder = lambda parser: parser
     extra_args_formatter = lambda args: ''
     max_ent_len_retrieve = lambda train_doc_store, dev_doc_store, args: 1
@@ -74,13 +75,13 @@ if __name__ == '__main__':
         'truncate': False
     }
     ent_retrieve_params = {}
-    tensor_dict_map = {'rel_seq': 'rel_seq',
-                       'seq_len': 'seq_len',
-                       'rel_len': 'rel_len',
-                       'target_rel': 'target_rel'}
-    run_model(visible_device_list=visible_device_list, visible_devices=visible_devices,
-              memory_fraction=memory_fraction, model_name=model_name, extra_parser_args_adder=extra_parser_args_adder,
-              extra_args_formatter=extra_args_formatter, max_ent_len_retrieve=max_ent_len_retrieve,
-              max_rel_len_retrieve=max_rel_len_retrieve, rel_retrieve_params=rel_retrieve_params,
-              ent_retrieve_params=ent_retrieve_params, tensor_dict_map=tensor_dict_map,
-              model_params_generator=model_params_generator)
+
+    tensor_dict_map = {
+        'rel_seq': 'rel_seq',
+        'seq_len': 'seq_len',
+        'rel_len': 'rel_len',
+        'target_rel': 'target_rel'}
+
+    run_model(visible_device_list, visible_devices, memory_fraction, model_name, extra_parser_args_adder,
+              extra_args_formatter, max_ent_len_retrieve, max_rel_len_retrieve, rel_retrieve_params,
+              ent_retrieve_params, tensor_dict_map, model_params_generator)
